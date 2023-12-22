d0b39b9b4b3f2e0c9c4e33f71841a606c1c15105
edymol Github <edymolinap@gmail.com>
2023-12-22 03:10:21 -0800

    feature: removed unused imports

--- src/main/java/xyz/deved/socialmedia/socialmedia/user/UserJpaResource.java
+++ src/main/java/xyz/deved/socialmedia/socialmedia/user/UserJpaResource.java
@@ -5,10 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import xyz.deved.socialmedia.socialmedia.service.UserService;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
